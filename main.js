06826caa7d17da540b50b8c747ff1dc13aa00f67
Vladimir Timofeev <vovamium.timofeev@gmail.com>
2017-02-04 14:46:07 +0200

    new implimintation of table taske with buttons

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', ready, false);
+
+function ready() {
+    var arrayofus = [
+        new StudentsList('Alex', 'answer_quick', 'Male', true,'read books'),
+        new StudentsList('Irina', 'kitty', 'Female',true, 'play with rabbit'),
+        new StudentsList('Polina', 'candy_polya', 'Female', true,'sweeming'),
+        new StudentsList('Svetlana', 'sweetie_sun', 'Female', true, 'cooking'),
+        new StudentsList('Elena', 'smile_soul', 'Female',true,'play anagrams'),
+        new StudentsList('Vladimir', 'vovanium', 'Male',true,'play sport')
+
+    ];
+    //create an instance of StudentsList
+    var creator = new View(arrayofus);
+    // show the table
+    creator.showTable(arrayofus);
+   
+    var table = document.querySelector('table');
+
+    table.addEventListener('click', ShowHobby, false);
+
+    function ShowHobby(event) {
+        var target = event.target; 
+        if (target.tagName != 'BUTTON') return; 
+        target.nextElementSibling.classList.toggle('show');
+    }
+}
