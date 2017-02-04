9eca25b542c8f1287a9ad7f05bdf75c6bcb99bb9
Vladimir Timofeev <vovamium.timofeev@gmail.com>
2017-02-05 00:20:22 +0200

    remove duplicate file

--- StudentsList.js
+++
@@ -1,7 +0,0 @@
-function StudentsList(name, skype, gender, havehobby,hobby) {
-    this.name = name;
-    this.skype = skype;
-    this.gennder = gender;
-    this.havehobby = havehobby;
-    this.hobby = hobby;
-}
